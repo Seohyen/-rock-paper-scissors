59b48a63995520c50e4df83e772484e0f5a8834e
SeoHyun <namja4097@gmail.com>
2023-03-07 09:49:29 +0900

    Start note 이동부터

---
+++ Assets/Script/Rps.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rps : MonoBehaviour
+{
+
+    public GameObject[] rival;
+
+    public GameObject playerPaper;
+    public GameObject playerRock;
+    public GameObject playerScissor;
+    
+    private GameObject playerChoice; 
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
